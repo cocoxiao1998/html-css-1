1a41e1810dc387ea4ddd5e0af939e2b5e009789c
Coco Xiao <cocoxiao1998@yahoo.com>
2019-09-18 20:40:42 -0400

    Moved genre code to its own folder and files. Added a nav bar that will toggle between invisible and visible when changing vp size. Still need drop down functionality though.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const NAV = document.querySelectorAll("nav a")
+const NAV_A = document.querySelectorAll("nav a")
+const NAV_BAR = document.querySelector(".nav-bar")
 var size = window.matchMedia("(max-width: 899px)")
 
 function reveal(size) {
     if (size.matches) { 
-        for (var i = 0; i < NAV.length; i++) {
-            NAV[i].classList.add("hide")
+        for (var i = 0; i < NAV_A.length; i++) {
+            NAV_A[i].classList.add("hide")
         }
+        NAV_BAR.classList.remove("hide")
     } else {
-        for (var i = 0; i < NAV.length; i++) {
-            NAV[i].classList.remove("hide")
+        for (var i = 0; i < NAV_A.length; i++) {
+            NAV_A[i].classList.remove("hide")
         }
+        NAV_BAR.classList.add("hide")
     }
 }
   
-//reveal(size) // call listener function at run time
+reveal(size) // call listener function at run time
 size.addListener(reveal) // attach listener function on state changes
 
 
