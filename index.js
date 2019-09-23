b0dcb157f5c9bb8bffbf01e3dfed7e86405d5de8
Coco Xiao <cocoxiao1998@yahoo.com>
2019-09-23 18:27:50 -0400

    Added some functionality for nav bar drop down

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const NAV_A = document.querySelectorAll("nav a")
+const NAV_A_LENGTH = 4
 const NAV_BAR = document.querySelector(".nav-bar")
+const DROP_DOWN_CONTENT = document.querySelectorAll(".drop-down-content a")
 var size = window.matchMedia("(max-width: 899px)")
+
+function drop_down() {
+    for (var i = 0; i < DROP_DOWN_CONTENT.length; i++) {
+        DROP_DOWN_CONTENT[i].classList.toggle("hide")
+    }
+}
 
 function reveal(size) {
     if (size.matches) { 
-        for (var i = 0; i < NAV_A.length; i++) {
+        for (var i = 0; i < NAV_A_LENGTH; i++) {
             NAV_A[i].classList.add("hide")
         }
+
         NAV_BAR.classList.remove("hide")
     } else {
-        for (var i = 0; i < NAV_A.length; i++) {
+        for (var i = 0; i < NAV_A_LENGTH; i++) {
             NAV_A[i].classList.remove("hide")
         }
+
         NAV_BAR.classList.add("hide")
     }
 }
-  
+
+drop_down()
 reveal(size) // call listener function at run time
 size.addListener(reveal) // attach listener function on state changes
+  
 
 
