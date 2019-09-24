1e8a80a075e378f46b41100c75739d659e0eaa4a
Coco Xiao <cocoxiao1998@yahoo.com>
2019-09-24 16:13:27 -0400

    Clicking window function works properly now. Added the files back together. This site will just be this one page.

--- index.js
+++ index.js
@@ -10,18 +10,35 @@ function drop_down() {
 
 // hiding or displaying the different links to sections when changing the display size
 function reveal(size) {
+    // for width: 899px or below
     if (size.matches) { 
         for (var i = 0; i < NAV_A_LENGTH; i++) {
             NAV_A[i].classList.add("hide")
         }
 
         NAV_BAR.classList.remove("hide")
-    } else {
+    } else { // for width above: 899px
         for (var i = 0; i < NAV_A_LENGTH; i++) {
             NAV_A[i].classList.remove("hide")
         }
-
+        
         NAV_BAR.classList.add("hide")
+
+        if(!DROP_DOWN_CONTENT.classList.contains("hide")) {
+            DROP_DOWN_CONTENT.classList.toggle("hide")
+        }
+    }
+}
+
+// if the window is clicked, dropdown menu options will disappear
+window.onclick = function(event) {
+    console.log("testing 1")
+    if (!event.target.matches(".nav-bar")) {
+        console.log("testing 2")
+        if(!DROP_DOWN_CONTENT.classList.contains("hide")) {
+            DROP_DOWN_CONTENT.classList.toggle("hide")
+            console.log("testing 3")
+        }
     }
 }
 
